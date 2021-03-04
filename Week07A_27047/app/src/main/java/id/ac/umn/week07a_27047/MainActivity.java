b71e77ecb96d3d877e51aabe5ec55156ab6ba990
laitsky <vincent.diamond15@gmail.com>
2021-03-04 15:51:10 +0700

    w7 leftover files

--- Week07A_27047/app/src/main/java/id/ac/umn/week07a_27047/MainActivity.java
+++ Week07A_27047/app/src/main/java/id/ac/umn/week07a_27047/MainActivity.java
@@ -1,14 +1,75 @@
 package id.ac.umn.week07a_27047;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button foto;
+    private Button video;
+    private ImageView kotakFoto;
+    private VideoView kotakVideo;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_VIDEO_CAPTURE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        foto = (Button) findViewById(R.id.button);
+        video = (Button) findViewById(R.id.button2);
+        kotakFoto = (ImageView) findViewById(R.id.imageView);
+        kotakVideo = (VideoView) findViewById(R.id.videoView);
+
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(kotakVideo);
+        kotakVideo.setMediaController(controller);
+
+        foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_VIDEO_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitMap = (Bitmap) extras.get("data");
+            kotakFoto.setImageBitmap(imageBitMap);
+        } else if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            kotakVideo.setVideoURI(videoUri);
+            kotakVideo.seekTo(100);
+            kotakVideo.start();
+        }
     }
 }
